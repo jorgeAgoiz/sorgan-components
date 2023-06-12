d465408758a621cc346a960021062cc8c861c2da
jorgeAgoiz <jorgeagoiz@gmail.com>
2023-06-12 18:39:27 +0200

    Config package.json core package to lib mode

--- core/vite.config.ts
+++ core/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { resolve } from "node:path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "sorgan-components",
+      formats: ["es", "umd"],
+      fileName: (format) => `sorgan-components.${format}.js`,
+    },
+  },
+});
