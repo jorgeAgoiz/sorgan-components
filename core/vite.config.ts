19dfa35e85c35aa7825943d6fe96cd5291b7d8ee
jorgeAgoiz <jorgeagoiz@gmail.com>
2023-06-12 20:24:23 +0200

    More configs updated

--- core/vite.config.ts
+++ core/vite.config.ts
@@ -1,10 +1,16 @@
 import react from "@vitejs/plugin-react";
 import { resolve } from "node:path";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    dts({
+      insertTypesEntry: true,
+    }),
+    react(),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
