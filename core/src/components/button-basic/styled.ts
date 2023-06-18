e2dd3d6237ed758d4946b1aabeeb40e0a2573861
jorgeAgoiz <jorgeagoiz@gmail.com>
2023-06-18 12:49:33 +0200

    Small fix

--- core/src/components/button-basic/styled.ts
+++ core/src/components/button-basic/styled.ts
@@ -9,25 +9,30 @@ export const ButtonStyled = styled(Button)(
   ({ colorvariant }: ButtonStyledProps) => {
     let color: string;
     let borderColor: string;
+    let textColor: string;
 
     switch (colorvariant) {
       case "primary":
         color = "#f5eb41";
         borderColor = "#202020";
+        textColor = "#202020";
         break;
       case "secondary":
         color = "#202020";
         borderColor = "#f5eb41";
+        textColor = "white";
         break;
       default:
         color = "#f5eb41";
         borderColor = "#202020";
+        textColor = "#202020";
     }
 
     return {
       backgroundColor: color,
       borderColor: borderColor,
       borderWidth: "4px",
+      color: textColor,
     };
   }
 );
